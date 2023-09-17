39587a9e48db24abb0352d7fb813183f24d6011e
Bloom <bloomtauta@gmail.com>
2023-09-17 15:03:06 +0300

    done

---
+++ app/migrations/versions/2f926273c40a_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 2f926273c40a
+Revises: 
+Create Date: 2023-09-17 14:32:12.083281
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2f926273c40a'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
